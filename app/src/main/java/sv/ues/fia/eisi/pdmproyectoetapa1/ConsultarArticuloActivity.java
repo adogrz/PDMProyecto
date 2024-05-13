969a2cf48b67104b6e85279e5f22414905335db0
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2024-05-12 21:17:35 -0600

    Agregado activity para consultar un articulo

--- app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/ConsultarArticuloActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/ConsultarArticuloActivity.java
@@ -1,24 +1,73 @@
 package sv.ues.fia.eisi.pdmproyectoetapa1;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import sv.ues.fia.eisi.pdmproyectoetapa1.dao.DAOException;
+import sv.ues.fia.eisi.pdmproyectoetapa1.dao.sqlite.ControlBaseDatos;
+import sv.ues.fia.eisi.pdmproyectoetapa1.modelo.Articulo;
 
 public class ConsultarArticuloActivity extends AppCompatActivity {
+    EditText editIdArticulo;
+    TextView tvNombreArticulo, tvDescripcion, tvPrecioUnitario, tvStock, tvTipoArticulo,
+            tvLocalArticulo, tvProveedorArticulo;
+    Button btnMostrarDatosArticulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_consultar_articulo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.consular_articulo_activity), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        editIdArticulo = findViewById(R.id.etNombreArticulo);
+        tvNombreArticulo = findViewById(R.id.tvNombreArticulo);
+        tvDescripcion = findViewById(R.id.tvDescripcion);
+        tvPrecioUnitario = findViewById(R.id.tvPrecioUnitario);
+        tvStock = findViewById(R.id.tvStock);
+        tvTipoArticulo = findViewById(R.id.tvTipoArticulo);
+        tvLocalArticulo = findViewById(R.id.tvLocalArticulo);
+        tvProveedorArticulo = findViewById(R.id.tvProveedorArticulo);
+        btnMostrarDatosArticulo = findViewById(R.id.btnMostrarDatos);
+
+        btnMostrarDatosArticulo.setOnClickListener(v -> mostrarDatosArticulo());
+    }
+
+    private void mostrarDatosArticulo() {
+        ControlBaseDatos cBD = ControlBaseDatos.obtenerInstancia(ConsultarArticuloActivity.this);
+        Articulo articulo = null;
+        String nombreTipoArticulo = null;
+        String nombreProveedor = null;
+        String nombreLocal = null;
+
+        try {
+            articulo = cBD.getArticuloDAO().obtener(editIdArticulo.getText().toString());
+            nombreTipoArticulo = cBD.getTipoArticuloDAO().obtener(articulo.getIdTipoArticulo()).getNombre();
+            nombreProveedor = cBD.getProveedorDAO().obtener(articulo.getIdProveedor()).getNombre();
+            String idLocal = cBD.getLocalArticuloDAO().obtenerPorIdArticulo(articulo.getIdArticulo()).getIdLocal();
+            nombreLocal = cBD.getLocalDAO().obtener(idLocal).getNombreLocal();
+        } catch (DAOException e) {
+            Toast.makeText(ConsultarArticuloActivity.this, "No se encontró el artículo", Toast.LENGTH_SHORT).show();
+        }
+
+        if (articulo != null) {
+            tvNombreArticulo.setText(articulo.getNombre());
+            tvDescripcion.setText(articulo.getDescripcion());
+            tvPrecioUnitario.setText(String.valueOf(articulo.getPrecioUnitario()));
+            tvStock.setText(String.valueOf(articulo.getStock()));
+            tvTipoArticulo.setText(nombreTipoArticulo);
+            tvLocalArticulo.setText(nombreLocal);
+            tvProveedorArticulo.setText(nombreProveedor);
+        } else {
+            tvNombreArticulo.setText("");
+            tvDescripcion.setText("");
+            tvPrecioUnitario.setText("");
+            tvStock.setText("");
+            tvTipoArticulo.setText("");
+            tvLocalArticulo.setText("");
+            tvProveedorArticulo.setText("");
+        }
     }
 }
