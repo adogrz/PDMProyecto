eae6a6faac224550f28143d2fe0601ce3e705e29
Alejandro347 <alemonsan05@gmail.com>
2024-05-12 23:33:44 -0600

    Creacion de Activitys y Layouts para Receta Medica

---
+++ app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/RecetasActualizarActivity.java
@@ -0,0 +1,14 @@
+package sv.ues.fia.eisi.pdmproyectoetapa1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecetasActualizarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recetas_actualizar);
+    }
+}
