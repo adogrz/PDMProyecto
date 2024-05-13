d970f718e69cebbf2764c3abc020862daf54f394
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2024-05-12 23:34:53 -0600

    Agregado funcionalidad de eliminar articulo

--- app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/EliminarArticuloActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/EliminarArticuloActivity.java
@@ -1,24 +1,46 @@
 package sv.ues.fia.eisi.pdmproyectoetapa1;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import sv.ues.fia.eisi.pdmproyectoetapa1.dao.DAOException;
+import sv.ues.fia.eisi.pdmproyectoetapa1.dao.sqlite.ControlBaseDatos;
+import sv.ues.fia.eisi.pdmproyectoetapa1.modelo.Articulo;
 
 public class EliminarArticuloActivity extends AppCompatActivity {
+    EditText editIdArticulo;
+    Button buttonEliminarArticulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_eliminar_articulo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.eliminar_articulo_activity), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        editIdArticulo = findViewById(R.id.etIdArticuloEliminar);
+        buttonEliminarArticulo = findViewById(R.id.btnEliminarArticulo);
+
+        buttonEliminarArticulo.setOnClickListener(v -> eliminarArticulo());
+    }
+
+    public void eliminarArticulo() {
+        String idArticulo = editIdArticulo.getText().toString();
+        if (idArticulo.isEmpty()) {
+            editIdArticulo.setError("Campo obligatorio");
+        } else {
+            ControlBaseDatos cBD = ControlBaseDatos.obtenerInstancia(EliminarArticuloActivity.this);
+            Articulo articuloEliminar;
+            try {
+                articuloEliminar = cBD.getArticuloDAO().obtener(idArticulo);
+                cBD.getArticuloDAO().eliminar(articuloEliminar);
+                Toast.makeText(EliminarArticuloActivity.this, "Articulo eliminado exitosamente",
+                        Toast.LENGTH_SHORT).show();
+            } catch (DAOException e) {
+                Toast.makeText(EliminarArticuloActivity.this, "Error: " + e.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
