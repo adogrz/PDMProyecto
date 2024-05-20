06220981d6ea9a7aea8d46c61c5b329e50297661
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2024-05-20 16:13:04 -0600

    Agregado funcionalidad para insertar una nueva compra desde la ui

--- app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/ui/compra/MenuCompraActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/pdmproyectoetapa1/ui/compra/MenuCompraActivity.java
@@ -1,14 +1,32 @@
 package sv.ues.fia.eisi.pdmproyectoetapa1.ui.compra;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+
 import sv.ues.fia.eisi.pdmproyectoetapa1.R;
 
 public class MenuCompraActivity extends AppCompatActivity {
+    MaterialButton btnInsertarCompra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_compra);
+        btnInsertarCompra = findViewById(R.id.btn_insertar_comp);
+
+        btnInsertarCompra.setOnClickListener(v -> navigateToActivity(InsertarCompraActivity.class));
+    }
+
+    /**
+     * Metodo para navegar a una actividad
+     * @param targetActivity Actividad a la que se desea navegar
+     */
+    private void navigateToActivity(Class<?> targetActivity) {
+        Intent intent = new Intent(this, targetActivity);
+        startActivity(intent);
     }
 }
